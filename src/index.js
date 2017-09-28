46f486ea3bb9cf0486d00992327dba71f9487a2c
ZerrickStaples <zerrick.staples@gmail.com>
2017-09-27 20:32:24 -0400

    Wrote code to pass 3rd test.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 export function startTimeRate (startTime, bedTime) {
+
+    if (startTime < 5){
+        startTime = 5;
+    }
+
     var startTimeHours = (bedTime - startTime)
     return startTimeHours * 12;
 }
