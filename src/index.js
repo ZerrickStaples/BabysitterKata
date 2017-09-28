958f3ec83ff50198daa9a8b06cc882567bf38703
ZerrickStaples <zerrick.staples@gmail.com>
2017-09-27 20:14:17 -0400

    wrote first failing test.

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function startTime () {
+    return 0;
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
 import Mocha from 'mocha';
 import { expect } from 'chai';
+import { startTime } from '../src/index'
 
 describe("Canary test", () => {
     it("true to equal true", () => {
         expect(true).to.be.true;
     })
 })
+
+describe("Babysitter Kata", () => {
+    it("Calculate $12/hour for 1 hour", () => {
+        expect(startTime()).to.equal(12);
+    })
+})
+
